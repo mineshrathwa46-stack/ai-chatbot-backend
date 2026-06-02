6fe5249fef7b99c83805b290b206b19d658bc91f
Mineshrathva <minesrathwa@gmail.com>
2026-06-02 16:54:04 +0530

    update name

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,167 +1,100 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-
-let users = [];
+import db from "../config/firebase.js";
 
 // REGISTER
-export const registerUser =
-async (req, res) => {
-
+export const registerUser = async (req, res) => {
   try {
-
-    const { email, password } =
-      req.body;
-
-    const userExists =
-      users.find(
-        (user) =>
-          user.email === email
-      );
-
-    if (userExists) {
-
+    const { email, password,name } = req.body;
+    if (!email || !password || !name) {
       return res.status(400).json({
-        message:
-          "User already exists",
+        message: "All fields are required",
       });
+    }
+    const snapshot = await db
+      .collection("users")
+      .where("email", "==", email)
+      .get();
 
+    if (!snapshot.empty) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
     }
 
-    const hashedPassword =
-      await bcrypt.hash(
-        password,
-        10
-      );
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = {
-
+      name,
       email,
-
-      password:
-        hashedPassword,
-
+      password: hashedPassword,
     };
-
-    users.push(newUser);
-
+    await db.collection("users").add(newUser);
     // token generate
     const token = jwt.sign(
-
       {
         email: newUser.email,
       },
-
       process.env.JWT_SECRET,
-
       {
-        expiresIn: "1h",
-      }
-
+        expiresIn: "7d",
+      },
     );
-
     res.status(201).json({
-
-      message:
-        "User registered successfully",
-
+      message: "User registered successfully",
       token,
-
-      email:
-        newUser.email,
-
+      email: newUser.email,
     });
-
   } catch (error) {
-
     console.log(error);
 
     res.status(500).json({
-
-      message:
-        "Server error",
-
+      message: "Server error",
     });
-
   }
-
 };
-
 // LOGIN
-export const loginUser =
-async (req, res) => {
-
+export const loginUser = async (req, res) => {
   try {
+    const { email, password } = req.body;
 
-    const { email, password } =
-      req.body;
+    const snapshot = await db
+      .collection("users")
+      .where("email", "==", email)
+      .get();
 
-    const user =
-      users.find(
-        (u) => u.email === email
-      );
-
-    if (!user) {
-
-      return res.status(401).json({
-
-        message:
-          "User does not exist",
-
+    if (snapshot.empty) {
+      return res.status(404).json({
+        message: "User not found",
       });
-
     }
+    const userDoc = snapshot.docs[0];
 
-    const isMatch =
-      await bcrypt.compare(
-        password,
-        user.password
-      );
+    const user = userDoc.data();
+    const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-
       return res.status(401).json({
-
-        message:
-          "Invalid password",
-
+        message: "Invalid password",
       });
-
     }
 
     const token = jwt.sign(
-
-      {
-        email: user.email,
-      },
-
+      { email: user.email },
       process.env.JWT_SECRET,
-
       {
         expiresIn: "1h",
-      }
-
+      },
     );
-
     res.status(200).json({
-
       token,
 
-      email:
-        user.email,
-
+      email: user.email,
     });
-
   } catch (error) {
-
     console.log(error);
-
     res.status(500).json({
-
-      message:
-        "Server error",
-
+      message: "Server error",
     });
-
   }
-
 };
