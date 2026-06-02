ada7603c5bb7cc9e5a5d32669ea6e41af73c0fd5
Mineshrathva <minesrathwa@gmail.com>
2026-06-02 18:53:15 +0530

    fix firebase env and render deploy

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import "./config/env.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 
-import authRoutes
-from "./routes/authRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -11,18 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 // all routes
-app.use(
-  "/api",
-  authRoutes
-);
+app.use("/api", authRoutes);
 
-const PORT =
-  process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-
-  console.log(
-    `Server running on ${PORT}`
-  );
-
+  console.log(`Server running on ${PORT}`);
 });
